e89e640bac20a568deb608e004bf5954025402aa
Jon Wear <jon@dramafever.com>
2016-11-03 16:50:35 -0400

    and now we have caching

--- functions/dbcall/main.go
+++ functions/dbcall/main.go
@@ -12,10 +12,15 @@ import (
 	"database/sql"
 
 	"github.com/apex/go-apex"
+	"github.com/garyburd/redigo/redis"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var info *log.Logger
+
+const movieformat = "m:%v"
+
+var redisendpiont string
 
 func main() {
 	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
@@ -48,19 +53,73 @@ func main() {
 		dbinfo.DBName = rEvent.StageVars["dbname"]
 		dbinfo.Username = rEvent.StageVars["dbuser"]
 		dbinfo.Password = rEvent.StageVars["dbpassword"]
+		redisendpiont = rEvent.StageVars["redis"]
 		if datatype == "movie" {
 			var dbErr error
+			var redisError error
 			info.Println("Start DB Call Movie")
+			m, redisError = getMovieRedis(id)
+			if redisError != nil {
+				info.Println("Redis Error:", redisError)
+			}
+			if m.Title != "" {
+				return m, nil
+			}
 			m, dbErr = getMovieData(id, dbinfo)
 			if dbErr != nil {
 				info.Println("Error:", dbErr)
 				return nil, dbErr
+			}
+			saveErr := saveMovieToRedis(m, id)
+			if saveErr != nil {
+				info.Println("Error Redis:", saveErr)
 			}
 			info.Println("End DB Call Movie")
 		}
 		info.Println("End")
 		return m, nil
 	})
+}
+
+func getMovieRedis(id int) (Movie, error) {
+	var m Movie
+	c, err := redis.Dial("tcp", fmt.Sprintf("%s:6379", redisendpiont))
+	if err != nil {
+		info.Println("Redis Dial Error:", err, fmt.Sprintf("%s:6379", redisendpiont))
+		return m, err
+	}
+	defer c.Close()
+	data, err := redis.String(c.Do("GET", fmt.Sprintf(movieformat, id)))
+	if err != redis.ErrNil && err != nil {
+		return m, err
+	}
+	if len(data) == 0 {
+		return m, nil
+	}
+	jerr := json.Unmarshal([]byte(data), &m)
+	if jerr != nil {
+		return m, jerr
+	}
+	m.Source = "cache"
+	return m, nil
+}
+
+func saveMovieToRedis(m Movie, id int) error {
+	c, err := redis.Dial("tcp", fmt.Sprintf("%s:6379", redisendpiont))
+	if err != nil {
+		info.Println("Redis Dial Error:", err, fmt.Sprintf("%s:6379", redisendpiont))
+		return err
+	}
+	defer c.Close()
+	movieData, jerr := json.Marshal(&m)
+	if jerr != nil {
+		return jerr
+	}
+	_, errConn := c.Do("SETEX", fmt.Sprintf(movieformat, id), 10, string(movieData))
+	if errConn != nil {
+		return errConn
+	}
+	return nil
 }
 
 func getMovieData(id int, dbinfo DBInfo) (Movie, error) {
@@ -91,13 +150,16 @@ func getMovieData(id int, dbinfo DBInfo) (Movie, error) {
 			info.Println("ERROR Scan:", errScan)
 			return m, errScan
 		}
+		m.ID = mid
 		m.Title = title
 		m.Year = year
+		m.Source = "db"
 		c := Character{}
 		c.Name = character
 		c.ActorInfo = Actor{ID: actorid, LastName: lastname, FirstName: firstname}
 		m.Characters = append(m.Characters, c)
 	}
+
 	return m, nil
 }
 
@@ -109,9 +171,11 @@ type DBInfo struct {
 }
 
 type Movie struct {
+	ID         int         `json:"movie_id"`
 	Title      string      `json:"title"`
 	Year       int         `json:"year"`
 	Characters []Character `json:"characters"`
+	Source     string      `json:"source"`
 }
 
 type Character struct {
